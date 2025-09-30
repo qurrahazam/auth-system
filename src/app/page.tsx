4a5b0464203986c5cf1e75dcdd57035e05b148c0
qurrahazam <qurrahazam@gmail.com>
2025-09-30 18:10:52 +0500

    Added: Base file

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Auth System</h1>
+    </main>
+  );
+}
